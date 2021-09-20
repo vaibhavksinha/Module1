586b823b957756723ddc35388c3f1bd3b1d82e8a
vaibhavksinha <vaibhav.mayank26@gmail.com>
2021-09-20 15:11:56 +0530

    Object

--- src/UseObject.java
+++ src/UseObject.java
@@ -4,11 +4,15 @@
     double DSA;
     double DBMS;
     double JPL_Lab;
+
+    double avg() {
+        return (AM3 + JPL_Lab + DSA + DBMS + PCPF) / 5;
+    }
 }
 
 public class UseObject {
     public static void main(String[] args) {
-        double avg;
+        double avg1;
         Object a = new Object();
 
         a.AM3 = 8.5;
@@ -16,10 +20,9 @@ public class UseObject {
         a.DBMS= 8.7;
         a.DSA= 7.9;
         a.JPL_Lab=9.8;
+        avg1=a.avg();
 
-        avg=(a.AM3+a.JPL_Lab+a.DSA+a.DBMS+a.PCPF)/5;
-
-        System.out.println("Average of all the subject ="+avg);
+            System.out.println("Average of all the subject ="+avg1);
 
 
 
